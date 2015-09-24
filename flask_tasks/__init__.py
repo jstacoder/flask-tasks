3ecf94c9f76abbf9961197eb86eb2e2a8a363f9b
kyle roux <kyle@level2designs.com>
2015-09-24 14:34:27 -0700

    css , jinja and html additions

--- flask_tasks/__init__.py
+++ flask_tasks/__init__.py
@@ -2,6 +2,23 @@ from flask_router import FlaskRouter
 from flask_template_loader import FlaskTemplateLoader
 from flask_apps import FlaskApps
 from flask import Flask
+from flask_xxl.filters import date_pretty
+from commands import getoutput
+import json
+import os.path as op
+
+parent = op.realpath(op.dirname(__file__))
+
+get_assets = lambda: json.loads(getoutput('cd {0} && bower list -r -j -p --allow-root'.format(parent)))
+
+def assets(asset_types=None):
+    default_asset_types = ['css','js','less']
+    asset_types = asset_types if asset_types is not None else default_asset_types
+    _js = '<script src="{0}"></script>'
+    _css = '<link rel=stylesheet href="{0}" />'
+    fix_asset = lambda name: dict(js=_js,css=_css)[op.splitext(name)[-1][1:]].format(name)
+    assets = get_assets()
+    return '\n'.join(map(str,[fix_asset(assets[a]) for a in assets if filter(lambda x: map(lambda y: y.endswith(x),assets[a]),asset_types)]))
 
 class NoSettingsObjectError(Exception):
     pass
@@ -19,6 +36,10 @@ def get_app(settings=None,extensions=[],*args,**kwargs):
     apps = FlaskApps(app)
     loader = FlaskTemplateLoader(app)
 
+    app.jinja_env.globals['assets'] = assets
+    app.jinja_env.filters['date_pretty'] = date_pretty
+
     return app
 
 
+#print assets(['css'])
